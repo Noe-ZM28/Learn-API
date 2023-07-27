97fe837ee7bc163c7eef2d42b33bb0923496f8d8
Noe ZM <noe.zm2000@gmail.com>
2023-07-27 00:14:07 -0600

    se convierte a clase

--- main.py
+++ main.py
@@ -8,25 +8,46 @@ load_dotenv()
 
 API_KEY_1 = os.getenv('API_KEY_1')
 API_KEY_2 = os.getenv('API_KEY_2')
-
-url = "http://api.open-notify.org/iss-now.json"
+# url = "http://api.open-notify.org/iss-now.json"
 # url = "http://api.open-notify.org/astros.json"
 
-lat = 0
-lon = 0
+class wheater:
+    def __init__(self, lat, lon, API_KEY):
+        self.lat = lat
+        self.lon = lon
+        self.API_KEY = API_KEY
 
-url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&{API_KEY_1}"
+    def get_url_api(self):
+        url = f"https://api.openweathermap.org/data/2.5/weather?lat={self.lat}&lon={self.lon}&appid={self.API_KEY}"
+        return url
 
-response = requests.get(url)
+    def get_response_api(self):
+        url = self.get_url_api()
+        response = requests.get(url)
 
-code_response = response.status_code
-info_response = response.json()
+        code_response = str(response.status_code)
+        info_response = response.json()
+
+        # if code_response[0] == "2":pass
+        weather = info_response['weather'][0]['description']
+        where = info_response['name']
+        return where, weather
+
+    def get_weather(self):
+        try:
+            where, weather = self.get_response_api()
+            print(f"->How is the weather like today in {where}?\n->There are {weather}")
+        except requests.exceptions.RequestException as e:
+            print("Error while making the API request:", e)
 
 
-def json_to_text(response_object):
-    text = json.dumps(response_object, sort_keys=True, indent=4)
-    print(text)
+lat = 35.0000000000
+lon = 105.0000000000
 
+how = wheater(
+    lat=lat,
+    lon=lon,
+    API_KEY=API_KEY_1)
 
-json_to_text(info_response)
+how.get_weather()
 
