9e43a68c4aa70f392d0fc005e379866ae5963c57
Noé Zúñiga <noe.zm2000@gmail.com>
2023-09-06 20:06:06 -0600

    se añade api para ontener icono de clima y otra de prueba

--- a.py
+++ a.py
@@ -1,17 +1,13 @@
 from requests import get
 from datetime import datetime
 import time
+from pprint import pprint
+#&language=es
+# pagar despues para pruebas
 
 def get_response_api():
-    url = "http://api.open-notify.org/iss-now.json"
+    url = """http://api.positionstack.com/v1/reverse?access_key={API_KEY}&query=19.5990445,-99.2615386&limit=1"""
     response = get(url).json()
-    print("\n\tISS POSITION")
-    print("-"*30)
-    print("LAT: ", response["iss_position"]["longitude"])
-    print("LON:", response["iss_position"]["latitude"])
-    print(datetime.fromtimestamp(response["timestamp"]))
-    print("-"*30)
-    time.sleep(1)
+    pprint(response)
 
-while True:
-    get_response_api()
+get_response_api()
--- main.py
+++ main.py
@@ -56,16 +56,19 @@ class data_wheater:
 
         return weather_response
 
+    def get_response_api_weather_icon(self, name_icon:str)->str:
+        path_icon = f"https://openweathermap.org/img/wn/{name_icon}@2x.png"
+        return path_icon
+
     def get_response_api_geocoding(self, API_KEY:str, lat:float|int, lon:float|int, limit:int= 5, to_JSON:bool = True)->(Response | dict):
         url_api_geocoding = f"http://api.openweathermap.org/geo/1.0/reverse?lat={lat}&lon={lon}&limit={limit}&appid={API_KEY}"
 
         geocoding_response = self.get_response_api(url= url_api_geocoding, to_JSON= to_JSON)
 
-        return geocoding_response
+        return None if geocoding_response == [] else geocoding_response
 
     def get_response_api_info_countries(self, code_country:str, to_JSON:bool= True)->(Response | dict):
         url_decode_name = f"https://restcountries.com/v3.1/alpha/{code_country}"
-
         decode_name_response = self.get_response_api(url= url_decode_name, to_JSON= to_JSON)
 
         return decode_name_response
@@ -81,13 +84,15 @@ class data_wheater:
             lat=self.lat,
             lon=self.lon)
 
+        print("\n\n")
         name_location = response_api_weather["name"]
         print("nombre del lugar: ", name_location)
 
-        state_location = response_api_geocoding[0]["state"]
-        print("nombre del estado: ", state_location)
-
         weather_data_location = response_api_weather['weather'][0]
+
+        path_icon = self.get_response_api_weather_icon(weather_data_location["icon"])
+        print("icono del clima del lugar: ", path_icon)
+
         print("informacion del clima del lugar: ", weather_data_location)
 
         clouds_level_location = response_api_weather['clouds']["all"]
@@ -101,7 +106,8 @@ class data_wheater:
 
         response_api_decode_name = self.get_response_api_info_countries(code_country=code_country)
         data_country = response_api_decode_name[0]
-
+        state_location = response_api_geocoding[0]["state"]
+        print("nombre del estado: ", state_location)
         name_country = data_country["translations"]["cym"]["common"]
         print("Nombre del pais: ", name_country)
 
@@ -111,10 +117,11 @@ class data_wheater:
         print(f"\n\n\n{name_location}, {state_location}, {name_country} -> {weather_data_location['description']}\n")
 
 
-API_KEY = getenv('API_KEY')
+API_KEY_OPENWEATHER = getenv('API_KEY_OPENWEATHER')
+API_KEY_POSITIONSTACK = getenv('API_KEY_POSITIONSTACK')
 
-lat = 19.5990445
-lon = -99.2615386
+lat = 38.642388
+lon = 139.848588
 
 print("Latitud: ", lat)
 print("Longitud: ", lon)
@@ -123,9 +130,10 @@ print("\n")
 wheater = data_wheater(
     lat=lat,
     lon=lon,
-    API_KEY=API_KEY)
+    API_KEY=API_KEY_OPENWEATHER)
 
 wheater.get_data()
 
 ISS = data_ISS()
 ISS.get_response_api_ISS()
+
