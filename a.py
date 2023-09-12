1cab67eef6451c52ea4c243aaa7c8e22b9bf6ffc
Noé Zúñiga <noe.zm2000@gmail.com>
2023-09-11 19:39:35 -0600

    ejemlo de api de información de continentes

--- a.py
+++ a.py
@@ -1,13 +1,24 @@
-from requests import get
-from datetime import datetime
-import time
-from pprint import pprint
-#&language=es
-# pagar despues para pruebas
+import geopandas as gpd
+from shapely.geometry import Point
 
-def get_response_api():
-    url = """http://api.positionstack.com/v1/reverse?access_key={API_KEY}&query=19.5990445,-99.2615386&limit=1"""
-    response = get(url).json()
-    pprint(response)
+lat = 48.393581
+lon = -24.157794
 
-get_response_api()
+# Carga el archivo GeoJSON con los límites de los continentes
+world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+
+# Definir un punto con las coordenadas que deseas verificar
+coords = Point(lon, lat)  # Sustituye lon y lat con tus propias coordenadas
+
+# Verifica si el punto está dentro de los límites de cualquier continente
+inside_continent = False
+for index, row in world.iterrows():
+    if row['geometry'].contains(coords):
+        inside_continent = True
+        continent_name = row['continent']
+        break
+
+if inside_continent:
+    print(f"Las coordenadas están dentro del continente {continent_name}.")
+else:
+    print("Las coordenadas no están dentro de ningún continente.")
